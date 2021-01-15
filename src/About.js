59ef9d7ce701a99692a7b94f9ccc0e688d3e7b25
Marcelo Mendes <celinhofm@gmail.com>
2021-01-14 21:14:22 -0300

    Criação das primeiras Paginas e do Roteamento

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div className="about">
+            <h1>Garotos de programa</h1>
+            <p>Fala deles...</p>
+        </div>
+    );
+}
